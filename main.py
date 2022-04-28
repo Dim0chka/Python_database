f5a1a97fbabc4b1f64ec71023a6f05b353b419fd
Dim0chka <Bugrick2004@yandex.ru>
2022-04-28 19:00:27 +0300

    correct commit

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def УДАЛИТЬ(student: str):
 
 
 def СВОДКА(number: int):
-    #сводка
+    # сводка
     summary = {
         0: "student",
         1: "year",
@@ -58,7 +58,7 @@ def СВОДКА(number: int):
     # obj.items() - получает все элементы словаря и возращает обьект, который отображает список пар по ключу и их значению
     # [(key, value), (key, value), (key, value)]
     obj = json.loads(open("date.json", "r").read())
-    obj_sort = sorted(obj.items(), key=lambda x: x[1][summary[number]])
+    obj_sort = sorted(obj.items(), key=lambda x: x[1][summary[int(number)]])
 
     for x in obj_sort:
         student = x[0]
@@ -76,45 +76,37 @@ def СОХРАНИТЬ(name_file: str):
 
 
 def ЗАГРУЗИТЬ(name_file: str):
+    obj = json.loads(open("date.json", "r").read())
+    for student in obj:
+            УДАЛИТЬ(obj[student]["student"])
+
     file = (open(name_file, "r").read()).split("\n")
 
     for x in file: #проходимя по каждому элементу в массиве
         arr_student = x.split() #переводим в массив данные об ученике
         ДОБАВИТЬ(arr_student[0], arr_student[1], arr_student[2], arr_student[3], arr_student[4])
 
-    #     obj[arr_student[0]] = {
-    #         "student": arr_student[0],
-    #         "year": arr_student[1],
-    #         "sr_inf": arr_student[2],
-    #         "sr_math": arr_student[3],
-    #         "sr_phis": arr_student[4]
-    #     }
-    #
-    # open("date.json", "w").write(json.dumps(obj, indent=4))
-
-"Hi guys"
 
 while True:
     query = input().split(" ")
 
     if query[0] == "ДОБАВИТЬ":
-        ДОБАВИТЬ(str(query[1]), int(query[2]), float(query[3]), float(query[4]), float(query[5]))
-        # print((open("date.json", "r").read()))
+        ДОБАВИТЬ(query[1], int(query[2]), float(query[3]), float(query[4]), float(query[5]))
 
     elif query[0] == "ПОКАЗАТЬ":
-        ПОКАЗАТЬ(str(query[1]))
+        ПОКАЗАТЬ(query[1])
 
     elif query[0] == "УДАЛИТЬ":
-        УДАЛИТЬ(str(query[1]))
+        УДАЛИТЬ(query[1])
 
     elif query[0] == "СВОДКА":
         СВОДКА(int(query[1]))
 
     elif query[0] == "СОХРАНИТЬ":
-        СОХРАНИТЬ(str(query[1]))
+        СОХРАНИТЬ(query[1])
 
     elif query[0] == "ЗАГРУЗИТЬ":
-        ЗАГРУЗИТЬ(str(query[1]))
+        ЗАГРУЗИТЬ(query[1])
 
     else:
         break
